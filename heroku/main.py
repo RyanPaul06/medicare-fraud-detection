bfc1d6dc184cba4077cbf57abddac74784d55bb5
mohamed-lahna <mohamed.lahna@outlook.com>
2022-09-04 17:26:17 +0100

    add procfile + rename script

--- heroku/main.py
+++ heroku/main.py
@@ -116,7 +116,3 @@ def response(df : DataFrame):
         labels.append(predict(new_input))
    
     return {"labels" : labels}
-    
-if __name__ == '__main__':
-
-    uvicorn.run(app, host='127.0.0.1', port=5000)
