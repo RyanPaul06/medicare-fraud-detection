ca9cd327717bdc9d885da94928566743dc3d0b73
mohamed-lahna <mohamed.lahna@outlook.com>
2023-01-06 09:10:55 +0100

    update new render url

--- streamlit/main.py
+++ streamlit/main.py
@@ -199,7 +199,7 @@ with st.container():
         pass
     
     #Send request to API
-    url1 =  "https://medicare-fraud-backend.herokuapp.com/Fraud_detection_single"
+    url1 =  "https://medicare-fraud.onrender.com/Fraud_detection_single"
     if(submitted):
         if(check_form(tot_hcpcs,male_bene,tot_bene,tot_serv)):
             with st.spinner('Processing ...'):
@@ -269,7 +269,7 @@ with st.container():
         pass
     with col13:
         pass
-    url2 =  "https://medicare-fraud-backend.herokuapp.com/Fraud_detection_multiple"
+    url2 =  "https://medicare-fraud.onrender.com/Fraud_detection_multiple"
     if(uploaded):
         with st.spinner('Processing ...'):
             if uploaded_file is not None:
